e9f87896737fcf0feebd952213cf6c024b051d3e
v1xenn <ranaya.mahitsa@gmail.com>
2024-08-14 23:06:06 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // ini javascript
+
+console.log('linked success');
+
+function formValidate() {
+    let inputWeight = document.getElementById('input-weight').value;
+    let inputHeight = document.getElementById('input-height').value;
+    console.log(`isi inputan berat badan: ${inputWeight}`);
+    console.log(`isi inputan tinggi badan: ${inputHeight}`);
+
+    (inputWeight == '' || inputHeight == '') 
+        ? alert('Inputan Berat Badan kosong!')
+        : result(inputHeight, inputWeight);
+
+    console.log('form submitted');
+}
+
+function result(inputHeight, inputWeight) {
+    let converCmToM = inputHeight * 100;
+        let resultHeight = Math.pow(converCmToM, 2);
+        console.log(inputWeight / resultHeight);
+}
