030a0a8960dbe98abee1f447656ea83ca5b94841
v1xenn <ranaya.mahitsa@gmail.com>
2024-08-15 19:36:41 +0800

    final update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,67 @@
-// ini javascript
+// membuat fungsi kalkulator BMI 
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+let resultArea = document.querySelector(".comment");
 
-function formValidate() {
-    let inputWeight = document.getElementById('input-weight').value;
-    let inputHeight = document.getElementById('input-height').value;
-    console.log(`isi inputan berat badan: ${inputWeight}`);
-    console.log(`isi inputan tinggi badan: ${inputHeight}`);
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
-    (inputWeight == '' || inputHeight == '') 
-        ? alert('Inputan Berat Badan kosong!')
-        : result(inputHeight, inputWeight);
+// membuat fungsi perhitungan BMI 
+function calculate(){
+ // jika ada yang tidak diisi maka muncul tanda 'harus terisi semua!'
+  if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+    modal.style.display = "block";
+    modalText.innerHTML = `Harus terisi semua!`;
 
-    console.log('form submitted');
+  }else{
+    countBmi();
+  }
+
 }
 
-function result(inputHeight, inputWeight) {
-    let converCmToM = inputHeight * 100;
-        let resultHeight = Math.pow(converCmToM, 2);
-        console.log(inputWeight / resultHeight);
+// membuat perhitungan BMI 
+function countBmi(){
+  var p = [age.value, height.value, weight.value];
+  if(male.checked){
+    p.push("male");
+  }else if(female.checked){
+    p.push("female");
+  }
+// membuat rumus perhitungan BMI 
+  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+
+// membuat hasil dari perhitungan BMI    
+  var result = '';
+  if(bmi<18.5){
+    result = 'Underweight';
+     }else if(18.5<=bmi&&bmi<=24.9){
+    result = 'Normal';
+     }else if(25<=bmi&&bmi<=29.9){
+    result = 'Overweight';
+     }else if(30<=bmi&&bmi<=34.9){
+    result = 'Obesitas';
+     }
+
+resultArea.style.display = "block";
+document.querySelector(".comment").innerHTML = `Berat badan anda  <span id="comment">${result}</span>`;
+document.querySelector("#result").innerHTML = bmi.toFixed(2);
+
+}
+
+// Saat mengklik <span> (x), modal akan tertutup
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// menutup modal 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
